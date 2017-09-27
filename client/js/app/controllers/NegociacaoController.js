004f4b5b938786bf61e55f86fcdca194f27893f5
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-27 02:01:54 -0300

    Concluindo  o ajax no método importaNegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -31,6 +31,15 @@ class NegociacaoController{
         let xhr = new XMLHttpRequest();
 
         xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                    console.log('Obtendo as negociações do servidor.');
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.');
+                }
+            }
+        };
         xhr.send();
     }
 
