3a9cb26b279386e96af87002c9a97982b5a73bb9
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-27 02:17:16 -0300

    adicionando os dados na listaNegociacoes e avisando ao usuário com uma mensagem de sucesso

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -35,12 +35,12 @@ class NegociacaoController{
             if(xhr.readyState === 4) {
                 if(xhr.status === 200) {
                     JSON.parse(xhr.responseText)
-                        .map( objeto => new Negociacao(objeto.data, objeto.quantidade, objeto.valor))
+                        .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
                         .forEach( negociacao => this._listaNegociacoes.adiciona(negociacao));
-                        
+
                 } else {
-                    console.log('Não foi possível obter as negociações do servidor.');
-                    console.lgo(xhr.responseText);
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana';
                 }
             }
         };
