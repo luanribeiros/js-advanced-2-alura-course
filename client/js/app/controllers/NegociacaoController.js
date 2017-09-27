946604f874934bbb325f4052f0e633f38377da59
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-27 02:19:12 -0300

    ajustes no método importaNegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -37,6 +37,7 @@ class NegociacaoController{
                     JSON.parse(xhr.responseText)
                         .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
                         .forEach( negociacao => this._listaNegociacoes.adiciona(negociacao));
+                        this._mensagem.texto = 'Negociações importadas com sucesso';
 
                 } else {
                     console.log(xhr.responseText);
