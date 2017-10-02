4f3defacf6d2bd31e786b6d9be6057a1932015a9
Luan Ribeiro <luanribeiros@gmail.com>
2017-10-01 21:14:50 -0300

    Criando método para a 'semana anterior'

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -19,6 +19,25 @@ class NegociacaoService {
                 xhr.send();
     }
 
+    obterNegociacoesDaSemanaAnterior(cb) {
+        
+                let xhr = new XMLHttpRequest();
+                
+                        xhr.open('GET', 'negociacoes/anterior');
+                        xhr.onreadystatechange = () => {
+                            if(xhr.readyState === 4) {
+                                if(xhr.status === 200) {
+                                    JSON.parse(xhr.responseText)
+                                        .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                                } else {
+                                    console.log(xhr.responseText);
+                                    cb('Não foi possível obter as negociações da semana', null);
+                                }
+                            }
+                        };
+                        xhr.send();
+            }
+            
     obterNegociacoesDaSemanaRetrasada(cb) {
         
                 let xhr = new XMLHttpRequest();
