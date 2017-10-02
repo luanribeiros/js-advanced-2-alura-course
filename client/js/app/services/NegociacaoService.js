769f01201c89be09ce1bde434348a5bc27c88983
Luan Ribeiro <luanribeiros@gmail.com>
2017-10-01 21:13:08 -0300

    criando método para a 'semana retrasada'

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -18,4 +18,23 @@ class NegociacaoService {
                 };
                 xhr.send();
     }
+
+    obterNegociacoesDaSemanaRetrasada(cb) {
+        
+                let xhr = new XMLHttpRequest();
+                
+                        xhr.open('GET', 'negociacoes/retrasada');
+                        xhr.onreadystatechange = () => {
+                            if(xhr.readyState === 4) {
+                                if(xhr.status === 200) {
+                                    JSON.parse(xhr.responseText)
+                                        .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                                } else {
+                                    console.log(xhr.responseText);
+                                    cb('Não foi possível obter as negociações da semana', null);
+                                }
+                            }
+                        };
+                        xhr.send();
+            }
 }
