5b8736411184b245648b937759e36a4e8b671d2c
Luan Ribeiro <luanribeiros@gmail.com>
2017-10-03 23:59:37 -0300

    colocando promise na classe NegociacaoService

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -2,21 +2,27 @@ class NegociacaoService {
     
     obterNegociacoesDaSemana(cb) {
 
-        let xhr = new XMLHttpRequest();
-        
-                xhr.open('GET', 'negociacoes/semana');
-                xhr.onreadystatechange = () => {
-                    if(xhr.readyState === 4) {
-                        if(xhr.status === 200) {
-                            JSON.parse(xhr.responseText)
-                                .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                        } else {
-                            console.log(xhr.responseText);
-                            cb('Não foi possível obter as negociações da semana', null);
+        return new Promise( (resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            
+                    xhr.open('GET', 'negociacoes/semana');
+                    xhr.onreadystatechange = () => {
+                        if(xhr.readyState === 4) {
+                            if(xhr.status === 200) {
+
+                                resolve(JSON.parse(xhr.responseText)
+                                    .map( objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                            } else {
+                                console.log(xhr.responseText);
+                                reject('Não foi possível obter as negociações da semana');
+                            }
                         }
-                    }
-                };
-                xhr.send();
+                    };
+                    xhr.send();
+
+        });
+  
     }
 
     obterNegociacoesDaSemanaAnterior(cb) {
